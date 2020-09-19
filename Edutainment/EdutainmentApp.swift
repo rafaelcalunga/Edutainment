10fb36fd4621f53a2e669dc27db5f95636b79558
Rafael Calunga <rafaelcalunga@gmail.com>
2020-09-18 20:40:46 -0400

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Rafael Calunga on 2020-09-18.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
